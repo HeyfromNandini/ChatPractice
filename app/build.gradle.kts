4cf2c07b518fa52f16371cfa173bc148ca525e72
HeyfromNandini <nandinidsecs122@gst.sies.edu.in>
2024-06-12 14:37:36 +0530

    changes in gradle and phone authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
 
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
